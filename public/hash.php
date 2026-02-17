99883135657dd4f6f2b393d3950d30348f381d1e
Ulysses <ulyssesburdeniii@gmail.com>
2026-02-17 08:44:09 -0500

    Delete hash.php

--- public/hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "customer: " . password_hash('customerPass!', PASSWORD_DEFAULT) . "<br>";
-echo "tech: " . password_hash('techPass!', PASSWORD_DEFAULT) . "<br>";
-echo "admin: " . password_hash('adminPass!', PASSWORD_DEFAULT) . "<br>";
-//run this script once to generate the password hashes for the default users. Add the generated hashes to the database for the respective users, then you can delete this file.
